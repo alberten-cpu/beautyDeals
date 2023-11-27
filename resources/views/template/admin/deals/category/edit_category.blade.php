16d5042cb8e65111402fd75485a5c252201da7db
albert <alberteb2910@gmail.com>
2023-11-27 07:14:59 +0100

    switch

--- resources/views/template/admin/deals/category/edit_category.blade.php
+++ resources/views/template/admin/deals/category/edit_category.blade.php
@@ -8,11 +8,11 @@
                                       breadcrumbs='{"Home":"dashboard","Deals Category":"categories.index","Edit Deal Category":""}'/>
         <!-- /.content-header -->
 
-        <x-admin.ui.card-form title="Deal Category Details" form-route="categories.update" form-id="edit_deal_category" form-route-id="{{ $category->dealCategoryId }}">
+        <x-admin.ui.card-form title="Deal Category Details" form-route="categories.update" form-id="edit_deal_category" form-route-id="{{ $category->categoryId }}">
             <x-slot name="input">
                 <div class="mb-3">
                     <x-admin.ui.bootstrap-switch name="status" id="status" label="Status" onText="Active"
-                                                 offText="Inactive" :value="$category->dealCategoryStatus"/>
+                                                 offText="Inactive" :value="$category->categoryStatus"/>
                 </div>
                 <x-admin.ui.input label="Category name" type="text" name="category_name" id="category_name" add-class=""
                                   placeholder="" required :value="$category->dealCategoryName"/>
